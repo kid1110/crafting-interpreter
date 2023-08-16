42e0822d7a078e41c4f7d77b8dd7824f92eb4322
kid <1050109853@qq.com>
2023-08-16 18:06:04 +0800

    feat: prepare preparser function

--- jlox/tool/GenerateAst.java
+++ jlox/tool/GenerateAst.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
-    public static void main(String[] args) throws IOException{
-       
+    public static void main(String[] args) throws IOException {
+
         String outputDir = "./jlox";
         defineAst(outputDir, "Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
@@ -17,36 +17,55 @@ public class GenerateAst {
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
-        String path = outputDir + "/" + baseName+".java";
+        String path = outputDir + "/" + baseName + ".java";
         System.out.println(path);
         PrintWriter writer = new PrintWriter(path, "UTF-8");
         writer.println("import java.util.List;");
         writer.println();
-        writer.println("abstract class "+baseName+"{");
-        for(String type:types){
+        writer.println("abstract class " + baseName + "{");
+        deifneVisitor(writer, baseName, types);
+        for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
-            defineType(writer,baseName,className,fields);
+            defineType(writer, baseName, className, fields);
         }
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
         writer.println("}");
         writer.close();
     }
-    private static void defineType(PrintWriter writer,String baseName,String className,String fieldList){
-        writer.println("  static class "+ className +" extends "+baseName +" {");
-        writer.println("    "+className+"("+fieldList+") {");
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + " {");
+        writer.println("    " + className + "(" + fieldList + ") {");
 
         String[] fields = fieldList.split(", ");
-        for(String field: fields){
+        for (String field : fields) {
             String name = field.split(" ")[1];
-            writer.println("      this."+name+"="+name+";");
+            writer.println("      this." + name + "=" + name + ";");
         }
+        writer.println("    }");
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor){");
+        writer.println("    return visitor.visit"+className+baseName+"(this);");
         writer.println("    }");
 
         writer.println();
-        for(String field:fields){
-            writer.println("    final "+field+";");
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
         }
         writer.println("  }");
 
+    }
+
+    private static void deifneVisitor(PrintWriter writer, String basename, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + basename + "(" + typeName + " " + basename.toLowerCase() + ");");
+        }
+        writer.println("  }");
     }
 }
